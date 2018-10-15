eedd3a7e26f357436ec9aa42adfc08b469392c79
Veronica Flores <veronica.marie.flores@gmail.com>
2018-10-15 16:06:59 -0600

    pushing build

---
+++ js/chunk-2d22bd05.7812c085.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22bd05"],{f126:function(e,n,t){"use strict";t.r(n);var s=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},r=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"work"},[t("h1",[e._v("This is an work page")])])}],i=t("2877"),a={},c=Object(i["a"])(a,s,r,!1,null,null,null);c.options.__file="Work.vue";n["default"]=c.exports}}]);
+//# sourceMappingURL=chunk-2d22bd05.7812c085.js.map
