8a5db87e64e805a82dbdf219cafe64fca1d31c43
Veronica Flores <veronica.marie.flores@gmail.com>
2018-10-15 14:13:55 -0600

    pushing some stuff

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,64 @@
 <template>
-  <div id="app">
+  <div id="app" class="color2">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <nav class="navbar navbar-expand-md color1">
+        <a class="navbar-brand pr-3" href="/#/">  <img src="./assets/glamour1.png" width="150" height="150" alt="" class="img-round"/></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"><i class="fas fa-terminal"></i></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarColor01">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <router-link to="/">Home</router-link>
+            </li>&nbsp; &nbsp; 
+            <li class="nav-item">
+              <router-link to="/about">About</router-link>
+            </li>&nbsp; &nbsp;
+            <li class="nav-item">
+              <router-link to="/work">Work</router-link>
+            </li>&nbsp; &nbsp;
+          </ul>
+        </div>
+      </nav>
     </div>
+      
     <router-view/>
   </div>
 </template>
 
+<script>
+import { Slide } from "vue-burger-menu";
+export default {
+  name: "App"
+};
+</script>
+
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "VT323", monospace;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: black;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.nav-item {
+  font-size: x-large;
+}
+.nav-item a {
+  color: #1abb9c;
+}
+.nav-item a:hover {
+  color: #8b104e;
+}
+.navbar-toggler-icon {
+  color: black;
+}
+.img-round {
+  border-radius: 50%;
+}
+.color1 {
+  background-color: #f8f1d3;
+}
+.color2 {
+  background-color: #1abb9c;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,76 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container mt-3">
+      <div class="row body">
+        <div class="col-xs-12 col-md-4 img-bg border-r mt-1 mb-2"><h1>VERONICA FLORES</h1><h3>FULLSTACK DEVELOPER</h3></div>
+        <div class="col-xs-12 col-md-8">
+          <div class="row pl-3 d-flex justify-content-around">
+            <div class="col-7 img-about size border-r ma-w mt-1 mb-2"><h1>About Page</h1></div>
+            <div class="col-5 color3 size border-r ma-w mt-1 mb-2"></div>
+            <div class="col-5 color4 size border-r ma-w mt-3"></div>
+            <div class="col-7 size img-work border-r ma-w mt-3"><h3 class="work-text" hidden>Find More Here</h3></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid">
+      <footer class="row color2">
+        <div class="col-6 d-flex justify-content-center"><p>&copy; BLAh BAlh BlAh</p></div>
+        <div class="col-6 d-flex justify-content-center"><p>Home | About | Work </p></div>
+      </footer>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "home",
+  components: {}
+};
 </script>
+<style>
+.work-text:hover {
+  visibility: visible;
+}
+.img-bg {
+  background: url("../assets/pancakes-food-eat-breakfast-730922.jpeg") no-repeat
+    center center;
+  background-size: cover;
+  height: 42rem;
+}
+.img-about {
+  background: url("../assets/pexels-photo-1368495.jpeg") no-repeat;
+  background-size: cover;
+}
+.img-work {
+  background: url("../assets/musicVue.png") no-repeat center center;
+  background-size: cover;
+}
+.img-round {
+  border-radius: 50%;
+}
+.border-r {
+  border-radius: 20px;
+}
+.size {
+  height: 20rem;
+}
+.color1 {
+  background-color: #f8f1d3;
+}
+.color2 {
+  background-color: #1abb9c;
+}
+.color3 {
+  background-color: #8b104e;
+}
+.color4 {
+  background-color: #5f1854;
+}
+.ma-w {
+  max-width: 50%;
+}
+</style>
+ 
